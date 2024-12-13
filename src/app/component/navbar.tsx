77310cb2761d92b1bdfcc60a8eaa437232ac8dca
daffakhairyal <daffakhairyal@gmail.com>
2024-12-13 14:13:10 +0700

    navbar progress

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -1,5 +1,42 @@
-export default function Navbar(){
-    return(
-        <nav>This is Navbar</nav>
-    )
-}
+import Image, { StaticImageData } from 'next/image';
+import logo from '../../../public/logo/logo.png';
+
+const logoImage: StaticImageData = logo;
+
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between items-center font-semibold p-3 text-slate-100 ">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+            <Image 
+                src={logoImage} 
+                alt="Logo"
+                width={72} 
+                height={72}
+            />
+            </a>
+
+            <ul className='flex gap-x-5'>
+                <li>
+                    <a href="#" target="_blank" className='hover:text-slate-400 duration-300'>HOME</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank" className='hover:text-slate-400 duration-300'>ABOUT ME</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank" className='hover:text-slate-400 duration-300'>SERVICE</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank" className='hover:text-slate-400 duration-300'>PROJECT</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank" className='hover:text-slate-400 duration-300'>CONTACT ME</a>
+                </li>
+            </ul>
+            <button className='p-4 rounded-full shadow-md bg-white text-black hover:bg-slate-800 hover:text-white duration-300'>
+                LET'S COLLABORATE
+            </button>
+        </nav>
+    );
+};
+
+export default Navbar;
